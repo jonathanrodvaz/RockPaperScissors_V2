b0193cc2930f031a889a1e907f35a623af88e3f9
jonathanrodvaz <jonathanrodvazquez@gmail.com>
2023-10-15 13:45:18 +0200

    RockPaperScissors_V2

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# RockPaperScissors_V2
+Version alternativa* del proyecto "Rock, Paper, Scissors" en C#. Primera lección del curso de Udemy "C# Para Principiantes: Curso Práctico - C# en 15 Proyectos" por Gabriel Moroni.
+
+*: Ahora con gráficos punteros next-gen!
+ Se recomienda correr el programa con los siguientes specs:
+·Processor: AMD Ryzen 5 3600X, Intel i5-10600K.
+·Memory: 16 GB RAM.
+·Graphics: AMD Radeon RX 6800 XT, NVIDIA GeForce RTX 2080.
+·DirectX: Version 12.
+·Network: Broadband Internet connection.
+·Storage: 125 GB available space.
+·Additional Notes: SSD Required.
---
+++ RockPaperScissors_V2/Program.cs
@@ -0,0 +1,99 @@
+﻿Console.WriteLine();
+Console.WriteLine("\\/  \\/  \\/  \\/  \\/  \\/  \\/  \\/  \\/  \\/  \\/  \\/  \\/  \\/  \\/  \\/  \\/  \\/  \\/  \\/  \\/  \\/  \\/  \\/  \\/  \\/  \\/\r\n");
+Console.WriteLine("______           _          __ ______                           __  _____      _                        \r\n| ___ \\         | |        / / | ___ \\                         / / /  ___|    (_)                       \r\n| |_/ /___   ___| | __    / /  | |_/ /_ _ _ __   ___ _ __     / /  \\ `--.  ___ _ ___ ___  ___  _ __ ___ \r\n|    // _ \\ / __| |/ /   / /   |  __/ _` | '_ \\ / _ \\ '__|   / /    `--. \\/ __| / __/ __|/ _ \\| '__/ __|\r\n| |\\ \\ (_) | (__|   <   / /    | | | (_| | |_) |  __/ |     / /    /\\__/ / (__| \\__ \\__ \\ (_) | |  \\__ \\\r\n\\_| \\_\\___/ \\___|_|\\_\\ /_/     \\_|  \\__,_| .__/ \\___|_|    /_/     \\____/ \\___|_|___/___/\\___/|_|  |___/\r\n                                         | |                                                            \r\n                                         |_|  ");
+Console.WriteLine();
+
+while (true)
+{
+    Console.WriteLine("Are you ready to rock... and paper... and scissors!?");
+    Console.WriteLine();
+    Console.WriteLine("Lets Begin!!!");
+    Console.WriteLine();
+    Console.WriteLine("\\/  \\/  \\/  \\/  \\/  \\/  \\/  \\/  \\/  \\/  \\/  \\/  \\/  \\/  \\/  \\/  \\/  \\/  \\/  \\/  \\/  \\/  \\/  \\/  \\/  \\/  \\/\r\n");
+    var selectedChoice = SelectChoice();
+    var yourChoice = char.Parse(selectedChoice);
+
+    Console.WriteLine();
+    Console.WriteLine($"You selected {yourChoice}");
+
+    var opponentChoice = GetOponentChoice();
+
+    Console.WriteLine($"Rockmaster chose {opponentChoice}");
+    Console.WriteLine();
+
+    DecideWinner(opponentChoice, yourChoice);
+    Console.WriteLine();
+    Console.WriteLine("Do you want to play again?");
+    Console.WriteLine("Enter Y to play again or any other key to stop...");
+
+    var playAgain = Console.ReadLine();
+    if (playAgain?.ToLower() == "y")
+        continue;
+    else
+        Console.WriteLine("Rockmaster laughs and says: 'Guess you had enough of losing at Rock / Paper / Scissors...!'");
+    break;
+
+}
+
+string SelectChoice()
+{
+    Console.WriteLine();
+    Console.WriteLine("Choose R, P o S: [R]ock, [P]aper or [S]cissors: ");
+    var selectedChoice = Console.ReadLine();
+
+    if (selectedChoice?.ToLower() != "r" && selectedChoice?.ToLower() != "p" && selectedChoice?.ToLower() != "s")
+    {
+        Console.WriteLine("Please select only one letter: R, P, S");
+        selectedChoice = SelectChoice();
+    }
+    return selectedChoice;
+}
+
+char GetOponentChoice()
+{
+    char[] options = new char[] { 'R', 'P', 'S' };
+    Random random = new Random();
+    int randomIndex = random.Next(0, options.Length);
+
+    return options[randomIndex];
+}
+
+void DecideWinner(char opponentChoice, char yourChoice)
+{
+    if (opponentChoice == yourChoice)
+    {
+        Console.WriteLine("Tie!");
+        return;
+    }
+    switch (yourChoice)
+    {
+        case 'R':
+        case 'r':
+            if (opponentChoice == 'P')
+                Console.WriteLine("Paper beats Rock, Rockmaster wins!");
+            else if (opponentChoice == 'S')
+                Console.WriteLine("Rock beats Scissors, You win!");
+            break;
+
+        case 'P':
+        case 'p':
+            if (opponentChoice == 'R')
+                Console.WriteLine("Paper beats Rock, You win!");
+            else if (opponentChoice == 'S')
+                Console.WriteLine("Scissors beats Paper, Rockmaster wins!");
+            break;
+
+        case 'S':
+        case 's':
+            if (opponentChoice == 'R')
+                Console.WriteLine("Rock beats Scissors, Rockmaster wins!");
+            else if (opponentChoice == 'P')
+                Console.WriteLine("Scissors beats Paper, You win!");
+            break;
+        default:
+            break;
+
+    }
+
+
+}
